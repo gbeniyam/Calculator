dd2fcb4a0c3c329b26b6cc9e015cc7a45cb8f258
gbeniyam <46503971+gbeniyam@users.noreply.github.com>
2024-04-17 18:29:01 -0700

    Output calculation to file as an option

--- calc_funcs.py
+++ calc_funcs.py
@@ -1,35 +1,56 @@
+from enum import Enum
+
+class Print_Out(Enum):
+    No_Output = 0
+    Standard_Out = 1
+    File_Out = 2
+
 class Calc:
     def __init__(self) -> None:
         self.total = 0
 
-    def add(self, x, y, printify=False):
+    def add(self, x: int | float, y: int | float, printify=Print_Out.Standard_Out):
         self.total = x + y
-        if printify:
+        if printify == Print_Out.Standard_Out:
             print(f"{x} + {y} = {self.total}")
-        # print out the result if desired.
+        elif printify == Print_Out.File_Out:
+            with open("Calc_output.txt", "a") as f:
+                f.write(f"{x} + {y} = {self.total}\n")
 
-    def sub(self, x, y, printify=False):
+    def sub(self, x: int | float, y: int | float, printify=Print_Out.No_Output):
         self.total = x - y
-        if printify:
+        if printify == Print_Out.Standard_Out:
             print(f"{x} - {y} = {self.total}")
+        elif printify == Print_Out.File_Out:
+            with open("Calc_output.txt", "a") as f:
+                f.write(f"{x} + {y} = {self.total}\n")
 
-    def mult(self, x, y, printify=False):
+    def mult(self, x: int | float, y: int | float, printify=Print_Out.File_Out):
         self.total = x * y
-        if printify:
+        if printify == Print_Out.Standard_Out:
             print(f"{x} * {y} = {self.total}")
+        elif printify == Print_Out.File_Out:
+            with open("Calc_output.txt", "a") as f:
+                f.write(f"{x} * {y} = {self.total}\n")
 
-    def div(self, x, y, printify=False):
+    def div(self, x: int | float, y: int | float, printify=Print_Out.File_Out):
         if y != 0:
             self.total = x / y
-            if printify:
+            if printify == Print_Out.Standard_Out:
                 print(f"{x} / {y} = {self.total}")
+            elif printify == Print_Out.File_Out:
+                with open("Calc_output.txt", "a") as f:
+                    f.write(f"{x} / {y} = {self.total}\n")
         else:
             print("Error: Division by 0")
             return None
 
-    def pow(self, x, y, printify=False):
-        if printify:
+    def pow(self, x: int | float, y: int | float, printify=Print_Out.File_Out):
+        if printify == Print_Out.Standard_Out:
             self.total = x ** y
+        elif printify == Print_Out.File_Out:
+            with open("Calc_output.txt", "a") as f:
+                f.write(f"{x} ^ {y} = {self.total}\n")
 
     def print_total(self):
         print(f"Current total: {self.total}")
@@ -40,6 +61,6 @@ class Calc:
 """
 1. def print, print statement. abstract for a class
 2. TODO: func for taking sum, and output to a file
-3. find a FreeAPI endpoint that I could hit, get json data.
+3. TODO: find a FreeAPI endpoint that I could hit, get json data.
     a. thecocktaildb
 """
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from calc_funcs import Calc
 
 if __name__ == "__main__":
     calc = Calc()
-    calc.add(2,2, printify=True)
-    calc.sub(10,3, printify=True)
-    calc.mult(2,3, printify=True)
-    calc.div(20,4, printify=True)
-    calc.pow(10,2, printify=False)
+    calc.add(2.5,4.0)
+    calc.sub(10,3)
+    calc.mult(2,3)
+    calc.div(20,4)
+    calc.pow(10,2)
 
     calc.print_total()
     calc.clear()
